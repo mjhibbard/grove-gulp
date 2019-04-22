331c7ab2da2b821b6c0c71ae702f0f9a01bf624d
Michael Hibbard <veg.it.up@live.com>
2019-04-21 22:24:56 -0700

    add gulp-sass automation/config

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,27 @@ const source = require("gulp-sourcemaps");
 //const pump = require("pump");
 const ejs = require("gulp-ejs");
 
+// Sass config and compiler
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+
 // BrowserSync with "live reload" feature:
-const browserSync = require('browser-sync');
+const browserSync = require("browser-sync");
 const reload = browserSync.reload;
 
 //Image compression plugins:
-const imagemin = require('gulp-imagemin');
-const imageminPngQuant = require('imagemin-pngquant');
-const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+const imagemin = require("gulp-imagemin");
+const imageminPngQuant = require("imagemin-pngquant");
+const imageminJpegRecompress = require("imagemin-jpeg-recompress");
 
 // Paths for tasks:
 const paths = {
   styles: {
     src: "../../theGrove2.0/public/stylesheets/**/*.css",
+    dest: "assets/stylesheets/"
+  },
+  sassStyles: {
+    src: "../../theGrove2.0/public/stylesheets/**/*.scss",
     dest: "assets/stylesheets/"
   },
   scripts: {
@@ -47,13 +55,18 @@ function createErrorHandler(name) {
 
 //Clean up old files that are to be rebuilt:
 function clean() {
-  return del([ 'assets/partials', 'assets/stylesheets', 'assets/*.html', 'assets/*.js' ]);
+  return del([
+    "assets/partials",
+    "assets/stylesheets",
+    "assets/*.html",
+    "assets/*.js"
+  ]);
 }
 function cleanImages() {
-  return del(['assets/images']);
+  return del(["assets/images"]);
 }
 function cleanAll() {
-  return del(['assets']);
+  return del(["assets"]);
 }
 
 //EJS file combine and convert to HTML:
@@ -61,7 +74,7 @@ function serveEjs() {
   return gulp
     .src(paths.ejs.src)
     .on("error", createErrorHandler("gulp.src"))
-    .pipe(ejs({ msg: "Hello Gulp!"}, {}, { ext: ".html" }))
+    .pipe(ejs({ msg: "Hello Gulp!" }, {}, { ext: ".html" }))
     .on("error", createErrorHandler("gulp-ejs"))
     .pipe(gulp.dest(paths.ejs.dest))
     .on("error", createErrorHandler("gulp.dest"));
@@ -90,7 +103,7 @@ function styles() {
     .pipe(cleanCSS())
     .pipe(
       rename({
-        basename: "styles"
+        basename: "main"
         // suffix: ".min"
       })
     )
@@ -98,21 +111,34 @@ function styles() {
     .pipe(gulp.dest(paths.styles.dest));
 }
 
+function sassStyles() {
+  return gulp
+    .src(paths.sassStyles.src)
+    .on("error", createErrorHandler("gulp.src"))
+    .pipe(source.init())
+    .pipe(sass().on("error", sass.logError))
+    .on("error", createErrorHandler("gulp sass function"))
+    .pipe(cleanCSS())
+    //.pipe(rename({ basename: "stylesTwo" }))
+    .pipe(source.write())
+    .pipe(gulp.dest(paths.sassStyles.dest));
+}
+
 //Image compression (lossy and lossless):
 function images() {
   return gulp
     .src(paths.images.src)
     .on("error", createErrorHandler("gulp.src"))
-    .pipe(imagemin(
-      [
+    .pipe(
+      imagemin([
         imagemin.gifsicle(),
         imagemin.jpegtran(),
         imagemin.optipng(),
         imagemin.svgo(),
         imageminPngQuant(),
         imageminJpegRecompress()
-      ]
-    ))
+      ])
+    )
     .on("error", createErrorHandler("imagemin"))
     .pipe(gulp.dest(paths.images.dest))
     .on("error", createErrorHandler("gulp.dest"));
@@ -122,25 +148,29 @@ function images() {
 function serve() {
   browserSync({
     server: {
-      baseDir: 'assets'
+      baseDir: "assets"
     }
   });
-  gulp
-    .watch(paths.ejs.src, async function watcherEjs (){
-      await serveEjs();
-      reload();
-      return;
-    });
-  gulp.watch(paths.styles.src, async function watcherStyles (){
-      await styles();
-      reload();
-      return;
-    });
-  gulp.watch(paths.scripts.src, async function watcherScripts (){
-      await scripts();
-      reload();
-      return;
-    });
+  gulp.watch(paths.ejs.src, async function watcherEjs() {
+    await serveEjs();
+    reload();
+    return;
+  });
+  gulp.watch(paths.styles.src, async function watcherStyles() {
+    await styles();
+    reload();
+    return;
+  });
+  gulp.watch(paths.scripts.src, async function watcherScripts() {
+    await scripts();
+    reload();
+    return;
+  });
+  gulp.watch(paths.sassStyles.src, async function watcherSassStyles() {
+    await sassStyles();
+    reload();
+    return;
+  });
 }
 
 //Watch certain files for changes and other stuff:
@@ -148,6 +178,7 @@ function watch() {
   gulp.watch(paths.ejs.src, serveEjs);
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.sassStyles.src, sassStyles);
 }
 
 exports.clean = clean;
@@ -157,11 +188,19 @@ exports.serveEjs = serveEjs;
 exports.serve = serve;
 exports.images = images;
 exports.styles = styles;
+exports.sassStyles = sassStyles;
 exports.scripts = scripts;
 exports.watch = watch;
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts, serveEjs, serve));
-const buildMore = gulp.series(clean, images, gulp.parallel(styles, scripts, serveEjs, serve));
+const build = gulp.series(
+  clean,
+  gulp.parallel(styles, sassStyles, scripts, serveEjs, serve)
+);
+const buildMore = gulp.series(
+  cleanAll,
+  images,
+  gulp.parallel(styles, sassStyles, scripts, serveEjs, serve)
+);
 
-gulp.task('build', buildMore);
-gulp.task('default', build);
+gulp.task("build", buildMore);
+gulp.task("default", build);
